f118b493d710c6e62a276871c3bf8e9e229edab9
Siddhesh Patil <hello@siddheshpatil.in>
2022-03-17 22:36:23 +0530

    simple table format ready

---
+++ index.js
@@ -0,0 +1,54 @@
+const hello = {
+    f12345: "Worldwide Helpline",
+    f12346: "Birds and Animal Helpline",
+    f12347: "De Addiction Helpline",
+    f12348: "Food Helpline",
+    f12349: "Child Helpline",
+    f12350: "Senior Citizen Helpline",
+    f12351: "Online Doctor Consultation",
+    f12352: "Online Lab Booking",
+    f12353: "Ambulance Helpline",
+    f12355: "Plasma Helpline",
+}
+
+function changeHeading(heading) {
+    const mainh1 = document.getElementById("mainh1");
+    mainh1.innerHTML = heading;
+}
+
+function insertData(arr) {
+    const table = document.getElementById("table");
+    table.innerHTML = "";
+    const phil = document.createElement("tr");
+    const claire = document.createElement("th");
+    const claire1 = document.createElement("th");
+    claire.innerHTML = "Organization";
+    claire1.innerHTML = "Contact Details";
+    phil.appendChild(claire);
+    phil.appendChild(claire1);
+    table.appendChild(phil);
+    
+    for (let i = 0; i < arr.length; i++) {
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        td1.innerHTML = arr[i].org;
+        td2.innerHTML = arr[i].contact;
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        table.appendChild(tr);
+    }
+}
+
+async function renderData(e) {
+    const id = `f${e.target.id}`;
+    const param = hello[id];
+    changeHeading(param);
+    console.log(param);
+    const url = `https://helpline-services-server.herokuapp.com/helplinenumber/?o=${param}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const arr = data.data;
+    insertData(arr)
+    console.log(data)
+}
--- main.js
+++ main.js
@@ -1,77 +1,77 @@
-async function fetchData(parent) {
-  try {
-    let res = await fetch(
-      `https://helpline-services-server.herokuapp.com/helplinenumber/?o=${parent}`,
-      {
-        method: "GET",
-        mode: "cors",
-      }
-    );
-    const data = res.json();
+// async function fetchData(parent) {
+//   try {
+//     let res = await fetch(
+//       `https://helpline-services-server.herokuapp.com/helplinenumber/?o=${parent}`,
+//       {
+//         method: "GET",
+//         mode: "cors",
+//       }
+//     );
+//     const data = res.json();
 
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-async function renderDataInTable() {
-  const data = await fetchData("Worldwide%20Helpline");
-  console.log(data);
-}
-
-async function renderTable(e) {
-  const model = {
-    datamodel:[
-    {
-      index: "worldwide",
-      name: "Worldwide%20Helpline"
-    },
-  ]
-};
-  var param = "hello";
-  const id = e.target.id;
-  // model.datamodel.forEach((item) => {
-  //   if (item.index == id) {
-  //     param = item.name
-  //     break;
-  //   }
-  // })
-  const data = await fetchData(param);
-  console.log(id);
-}
-
-// function changeHeading(headingdata) {
-//   let h1 = document.getElementById("mainh1");
-//   h1.innerHTML = headingdata;
-// }
-// function insertData(data) {
-//   const { org, contact } = data;
-//   let main = document.getElementById("rootfinal");
-//   let row = document.createElement("tr");
-//   row.innerHTML = `<td>${org}</td><td>${contact}</td>`;
-//   main.appendChild(row);
-// }
-
-// function renderTableData(names) {
-//   const hell = data["indianhelpline.com"];
-//   let tabledata = document.getElementById("rootfinal");
-//   tabledata.innerHTML = `<tr>
-//   <th>Organization</th>
-//   <th>Contact Details</th>
-// </tr>`;
-//   for (let i = 0; i < hell.length; i++) {
-//     if (hell[i].parent === names) {
-//       insertData(hell[i]);
-//     }
+//     return data;
+//   } catch (error) {
+//     console.log(error);
 //   }
-//   changeHeading(`${names} Services Contact Directory`);
 // }
 
-// function renderWorldwideHelpline() {
-//   renderTableData("Worldwide Helpline");
+// async function renderDataInTable() {
+//   const data = await fetchData("Worldwide%20Helpline");
+//   console.log(data);
 // }
 
-// function hello() {
-//   console.log(testdata.Employees[0].emailAddress);
+// async function renderTable(e) {
+//   const model = {
+//     datamodel:[
+//     {
+//       index: "worldwide",
+//       name: "Worldwide%20Helpline"
+//     },
+//   ]
+// };
+//   const id = e.target.id;
+//   const param = id;
+//   // model.datamodel.forEach((item) => {
+//   //   if (item.index == id) {
+//   //     param = item.name
+//   //     break;
+//   //   }
+//   // })
+//   const data = await fetchData(param);
+//   console.log(id);
 // }
+
+// // function changeHeading(headingdata) {
+// //   let h1 = document.getElementById("mainh1");
+// //   h1.innerHTML = headingdata;
+// // }
+// // function insertData(data) {
+// //   const { org, contact } = data;
+// //   let main = document.getElementById("rootfinal");
+// //   let row = document.createElement("tr");
+// //   row.innerHTML = `<td>${org}</td><td>${contact}</td>`;
+// //   main.appendChild(row);
+// // }
+
+// // function renderTableData(names) {
+// //   const hell = data["indianhelpline.com"];
+// //   let tabledata = document.getElementById("rootfinal");
+// //   tabledata.innerHTML = `<tr>
+// //   <th>Organization</th>
+// //   <th>Contact Details</th>
+// // </tr>`;
+// //   for (let i = 0; i < hell.length; i++) {
+// //     if (hell[i].parent === names) {
+// //       insertData(hell[i]);
+// //     }
+// //   }
+// //   changeHeading(`${names} Services Contact Directory`);
+// // }
+
+// // function renderWorldwideHelpline() {
+// //   renderTableData("Worldwide Helpline");
+// // }
+
+// // function hello() {
+// //   console.log(testdata.Employees[0].emailAddress);
+// // }
